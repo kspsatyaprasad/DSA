517cd0b5d0abb4c9a056f00b714e230065aad5d0
satyasolves <satyasolvesolutions@gmail.com>
2024-01-02 23:47:06 -0600

    learnt problem 2 in dsa

---
+++ Problem2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Problem2 {
+    //This is the Program for average of an array (Iterative and Recursive)
+    // Method to compute the average of elements in an array
+    static double computeAverage(int[] numbers, int totalElements) {
+
+        // Accumulating the sum of the array elements
+        int totalSum = 0;
+        for (int index = 0; index < totalElements; index++) {
+            totalSum += numbers[index];
+        }
+
+        // Calculating the average
+        return (double) totalSum / totalElements;
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+
+        int[] sampleNumbers = {10, 2, 3, 4, 5, 6, 7, 8, 9};
+        int length = sampleNumbers.length;
+
+        System.out.println("The average is: " + computeAverage(sampleNumbers, length));
+    }
+}
